be3ad09db62fc4c981addb1ffff340dea6658753
Matt Rosensweig <matthew.rosensweig@gmail.com>
2021-02-05 20:24:34 -0500

    Fix Poll Name Next button

--- src/components/PollNameForm/PollNameForm.js
+++ src/components/PollNameForm/PollNameForm.js
@@ -1,22 +1,29 @@
 import React from 'react';
 
 class PollNameForm extends React.Component {
+  handleClickNext = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
     const { error } = this.props;
 
     return (
       <section>
         {error && <p className="error">{error}</p>}
-        <label htmlFor="pollName">Poll Name: </label>
-        <input
-          type="text"
-          name="pollName"
-          id="pollName"
-          onChange={this.props.handleChangePollName}
-          defaultValue={this.props.values.pollName}
-        />
-        <br />
-        <button type="button" onClick={this.props.nextStep}>Next</button>
+        <form onSubmit={this.handleClickNext}>
+          <label htmlFor="pollName">Poll Name: </label>
+          <input
+            type="text"
+            name="pollName"
+            id="pollName"
+            onChange={this.props.handleChangePollName}
+            defaultValue={this.props.values.pollName}
+          />
+          <br />
+          <button type="submit">Next</button>
+        </form>
       </section>
     );
   }
